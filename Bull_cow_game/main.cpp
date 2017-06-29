a8f7ad89df8d0e04809b6f3e4373a2d85ca5b26f
Alfie <alfiewhitson31@gmail.com>
2017-06-29 21:28:15 +0100

    initial commit

---
+++ Bull_cow_game/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	constexpr int WORD_LENGTH = 27;
+	cout << "Welcome to bulls & cows game \n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter constant that I'm thinking of ?" << "\n";
+	return 0;
+}
