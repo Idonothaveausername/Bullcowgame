144280abf6ad242278c3de8878c905d8dc154a01
Alfie <alfiewhitson31@gmail.com>
2017-07-06 18:39:46 +0100

    things happened. deal with it

--- Bull_cow_game/main.cpp
+++ Bull_cow_game/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,5 +8,20 @@ int main()
 	constexpr int WORD_LENGTH = 27;
 	cout << "Welcome to bulls & cows game \n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter constant that I'm thinking of ?" << "\n";
+
+	// get a guess from the player
+	string Guess = "";
+	cout << "Enter ye guess : ";
+	getline(cin, Guess);
+
+	// repeat it back to them
+
+	cout << "you have guessed, " << Guess << endl;
+	cout << "Enter ye guess : ";
+	getline(cin, Guess);
+
+	// repeat it back to them
+
+	cout << "you have guessed, " << Guess << endl;
 	return 0;
 }
